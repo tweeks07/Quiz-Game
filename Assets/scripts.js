eb7e18ebfad8eec5ae0843da2568c5f9c63b4c66
Taylor Weeks <taylorlweeks707@gmail.com>
2021-02-18 20:08:27 -0500

    first commit

---
+++ Assets/scripts.js
@@ -0,0 +1,50 @@
+var startButton =document.getElementById("start")
+startButton.addEventListener('click', startGame)
+
+var score = 0; 
+
+
+function startGame () {
+    
+
+}
+var quizQuestions = [
+    {
+        Question: "An array is :",
+        a: "an ordered list of values",
+        b: "a TikTok dance",
+        c: "beams of sunshine",
+        answer: "A"
+    },
+    {
+        Question: "JavaScript is :",
+        a: "a nickname for coffee",
+        b: "programing language",
+        c: "none of the above",
+        answer: "B"
+    },
+    {
+        Question: "The HTML syntax for an image is :",
+        a: "<ing>",
+        b: "<DS2>",
+        c: "<img>",
+        answer: "C"
+    },
+    {
+        Question: "JavaScript _____ are used for storing and manipulating text ",
+        a: "Thread",
+        b: "String",
+        c: "Yarn",
+        answer: "B"
+    },
+    {
+        Question: "Which is a type of Pop up box available in JavaScript :",
+        a: "Alarm",
+        b: "Alert",
+        c: "Action",
+    }
+];
+
+
+
+
